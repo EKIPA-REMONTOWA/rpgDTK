744405bcc986496d650656c204ce9aadcd8c7dac
woxnioll <woxnioll@gmail.com>
2017-03-13 21:33:36 +0100

    v0.0.1
    Ola

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(800,600);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(400,300,60,0,2*Math.PI);
+ctx.stroke();
+
+function newGame(){
+	document.getElementById('controlls').style.display="none";
+	document.getElementById('newGame').style.display="block";
+	
+	
+}
+function SplayerCardS(){
+	document.getElementById('newGame').style.display="none";
+	document.getElementById('newGame2').style.display="block";
+}
